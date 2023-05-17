a9190e219d882a7a214059505fb02f820e61aab1
MD:HASIBUL ISLAM SHAON <102863709+hasibulislamshaon@users.noreply.github.com>
2023-05-17 19:12:12 +0600

    new

--- static/js/cart.js
+++ static/js/cart.js
@@ -1,1 +1,16 @@
-console.log("I am added")
+var updateBtns = document.getElementsByClassName('update-cart')
+
+for (i = 0; i < updateBtns.length; i++){
+    updateBtns[i].addEventListener('click', function(){
+        var productId = this.dataset.product
+        var action = this.dataset.action
+        console.log('productId:', productId, 'action:', action)
+
+        console.log('USER:',user)
+        if(user== 'AnonymousUser'){
+            console.log('Not Log in')
+        }else{
+            console.log("User is loged in, sending data....")
+        }
+    })
+}
